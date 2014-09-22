c1afef6fd6351fd83a1fae835fb27da5560d9843
fazerg <xamgelo@gmail.com>
2014-09-22 13:11:49 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,30 +14,30 @@
 # limitations under the License.
 
 # inherit from common msm8226-common
--include device/motorola/msm8226-common/BoardConfigCommon.mk
+-include device/ZTE/msm8226-common/BoardConfigCommon.mk
 
-LOCAL_PATH := device/motorola/falcon
+LOCAL_PATH := device/ZTE/X9180
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := xt1031,xt1032,xt1033,xt1034,falcon_umts,falcon_umtsds,falcon_cdma,falcon_retuaws,falcon,falcon_gpe
+TARGET_OTA_ASSERT_DEVICE := X9180,V9180,U9180,N9180
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-TARGET_KERNEL_CONFIG := falcon_defconfig
+TARGET_KERNEL_CONFIG := 9180_2209_defconfig
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_falcon.c
+TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_X9180.c
 TARGET_UNIFIED_DEVICE := true
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1023410176
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 5930614784
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00c00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00c00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x3E8000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x76C00000
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
 # inherit from the proprietary version
--include vendor/motorola/falcon/BoardConfigVendor.mk
+-include vendor/ZTE/X9180/BoardConfigVendor.mk
